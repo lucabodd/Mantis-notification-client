c6b13ce57f17de949b3c5b4191106cedf5131627
Luca Bodini <lucabodd@gmail.com>
2017-10-27 17:47:59 +0200

    Add files via upload

---
+++ config_sample.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "192.168.xx.xx";
+$username = "db_user";
+$password = "db_pass";
+$dbname = "db_name";
+// DEBUG or ERR
+$log_level= "ERR";
+$mantis_user="username used for login";
+?>
